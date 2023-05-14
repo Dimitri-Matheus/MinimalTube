18022b1fba66712e02d1dc4e0eb4e6cf0c8a402d
Dimitri Matheus de Oliveira <dimitrimatheusdeoliveira@gmail.com>
2023-05-13 22:16:24 -0400

    Update code
    - dark and light mode have been added
    - fixed the function to change the page

--- app/data.py
+++ app/data.py
@@ -1,9 +1,14 @@
-# fuction change page "home", "download" and "setting"
-def change_frame(frame_to_show, frames):
-    for frame in frames:
-        if frame == frame_to_show:
-            frame.grid(row=0, column=0, sticky='nsew')
-        else:
-            frame.grid_forget()
+# packages
+from tkinter import *
+import customtkinter as ctk
+from pytube import YouTube
 
+# change the theme to "light", "dark" and "system"
+def change_mode(value):
+    if value == 'light':
+        ctk.set_appearance_mode('light') #property for light mode
+    elif value == 'dark':
+        ctk.set_appearance_mode('dark') #property for dark mode
+    else:
+        ctk.set_appearance_mode('system') #property for system mode (the mode is chosen from the system)
 
--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@
 from tkinter import *
 import customtkinter as ctk
 import PIL.Image, PIL.ImageTk
-from pytube import YouTube
 from data import *
 
 # Colors
@@ -25,19 +24,9 @@ class MinimalTube(ctk.CTk):
         self.geometry('480x250')
         self.resizable(width=False, height=False)
 
-        # property the dark mode and light mode
+        # property the theme
         ctk.set_default_color_theme('dark-blue')
-        ctk.set_appearance_mode('dark')
-
-        # home frame
-        self.home = ctk.CTkFrame(self, corner_radius=0, width=480, height=250)
-        self.home.grid(row=0, column=0)
-
-        # video frame
-        self.video = ctk.CTkFrame(self, corner_radius=0, fg_color=gray_light, width=480, height=250)
-
-        # setting frame
-        self.setting = ctk.CTkFrame(self, corner_radius=0, fg_color=mercury, width=480, height=250)
+        ctk.set_appearance_mode('system')
 
         # load the images
         self.arrow = ctk.CTkImage(dark_image=PIL.Image.open('images/dark/push.png'), light_image=PIL.Image.open('images/light/push.png'), size=(45, 45))
@@ -53,38 +42,81 @@ class MinimalTube(ctk.CTk):
         self.dark = ctk.CTkImage(dark_image=PIL.Image.open('images/dark/moon.png'), light_image=PIL.Image.open('images/light/moon.png'), size=(32, 32))
         self.light = ctk.CTkImage(dark_image=PIL.Image.open('images/dark/sun.png'), light_image=PIL.Image.open('images/light/sun.png'), size=(32, 32))
 
-        # next page "video"
-        self.home_button_push = ctk.CTkButton(self.home, text=None, image=self.arrow, fg_color='transparent', hover=False, command=lambda: change_frame(self.video, [self.home, self.video]))
-        self.home_button_push.place(relx=1, rely=0.75, anchor=E)
+        # default button next page
+        self.button_push = ctk.CTkButton(self, text=None, image=self.arrow, fg_color='transparent', hover=False, command=lambda: change_buttons('video_download'))
+        self.button_push.place(relx=1, rely=0.75, anchor=E)
 
-        # next page "setting"
-        self.video_button_push = ctk.CTkButton(self.video, text=None, image=self.arrow, fg_color='transparent', hover=False, command=lambda: change_frame(self.setting, [self.video, self.setting]))
-        self.video_button_push.place(relx=1, rely=0.75, anchor=E)
+        # default buttons
+        self.button_1 = ctk.CTkButton(self, text=None, image=self.github, height=80, width=80)
+        self.button_1.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
+        self.button_1.place(relx=0.2, rely=0.1, anchor=N)
 
-        # next page "home"
-        self.setting_button_push = ctk.CTkButton(self.setting, text=None, image=self.arrow, fg_color='transparent', hover=False, command=lambda: change_frame(self.home, [self.setting, self.home]))
-        self.setting_button_push.place(relx=1, rely=0.75, anchor=E)
+        self.button_2 = ctk.CTkButton(self, text=None, image=self.instagram, height=80, width=80)
+        self.button_2.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
+        self.button_2.place(relx=0.4, rely=0.1, anchor=N)
 
-        # buttons social media
-        self.button_github = ctk.CTkButton(self.home, text=None, image=self.github, height=80, width=80)
-        self.button_github.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
-        self.button_github.place(relx=0.2, rely=0.1, anchor=N)
+        self.button_3 = ctk.CTkButton(self, text=None, image=self.share, height=80, width=80)
+        self.button_3.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
+        self.button_3.place(relx=0.6, rely=0.1, anchor=N)
 
-        self.button_instagram = ctk.CTkButton(self.home, text=None, image=self.instagram, height=80, width=80)
-        self.button_instagram.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
-        self.button_instagram.place(relx=0.4, rely=0.1, anchor=N)
-
-        self.button_share = ctk.CTkButton(self.home, text=None, image=self.share, height=80, width=80)
-        self.button_share.configure(fg_color=(white, shoe_wax), hover_color=(gray_light, gray), corner_radius=10)
-        self.button_share.place(relx=0.6, rely=0.1, anchor=N)
-
-        # about the project
-        self.title = ctk.CTkLabel(self.home, text='Name the project', text_color=(shoe_wax, white), font=ctk.CTkFont(size=16, weight='bold'))
+        # default labels
+        self.title = ctk.CTkLabel(self, text='MinimalTube', text_color=(shoe_wax, white), font=ctk.CTkFont(size=16, weight='bold'))
         self.title.place(relx=0.4, rely=0.5, anchor=N)
 
-        self.description = ctk.CTkLabel(self.home, text='Lorem Ipsum is simply\n dummy text of the printing\n and typesetting industry.', font=ctk.CTkFont(size=12, weight='bold'), justify='center')
+        self.description = ctk.CTkLabel(self, text='This program downloads youtube\n videos from a url.', font=ctk.CTkFont(size=12, weight='bold'), justify='center')
         self.description.configure(width=65, height=65, corner_radius=10, fg_color=(white, shoe_wax), text_color=(shoe_wax, white))
         self.description.place(relx=0.4, rely=0.75, anchor=CENTER)
+
+        self.search = ctk.CTkEntry(self, placeholder_text='YOUR URL HERE!', font=ctk.CTkFont(size=14, weight='bold'))
+        self.search.configure(width=230, height=40, corner_radius=10, justify='center', fg_color=(white, shoe_wax), border_color=(white, shoe_wax), text_color=(shoe_wax, white))
+        
+
+        # Function to change a page "home", "video_download" and "configuration"
+        def change_buttons(status):
+            if status == 'home':
+                # buttons
+                self.button_1.configure(image=self.github, command=None)
+                self.button_2.configure(image=self.instagram, command=None)
+                self.button_3.configure(image=self.share, command=None)
+
+                # about the project
+                self.title.configure(text='MinimalTube')
+                self.title.place(relx=0.4, rely=0.5, anchor=N)
+                self.description.place(relx=0.4, rely=0.75, anchor=CENTER)
+
+                # next page
+                self.button_push.configure(command=lambda: change_buttons('video_download'))
+
+            elif status == 'video_download':
+                # buttons
+                self.button_1.configure(image=self.find, command=None)
+                self.button_2.configure(image=self.resolution, command=None)
+                self.button_3.configure(image=self.download, command=None)
+
+                # about the project
+                self.title.configure(text='The video Name')
+                self.search.place(relx=0.4, rely=0.75, anchor=CENTER)
+                self.description.place_forget()
+
+                # next page
+                self.button_push.configure(command=lambda: change_buttons('configuration'))
+
+            elif status == 'configuration':
+                # buttons
+                self.button_1.configure(image=self.refresh, command=None)
+                self.button_2.configure(image=self.dark, command=lambda: change_mode('dark'))
+                self.button_3.configure(image=self.light, command=lambda: change_mode('light'))
+
+                # about the project
+                self.title.configure(text='Settings')
+                self.title.place(relx=0.4, rely=0.75, anchor=CENTER)
+                self.search.place_forget()
+
+                # next page
+                self.button_push.configure(command=lambda: change_buttons('home'))
+
+            else:
+                print('ERRO!')
 
 
     # Add icon to window

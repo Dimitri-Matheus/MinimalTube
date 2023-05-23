7e1286cc9ad1d498c918f762d52380a084fd40e5
Dimitri Matheus de Oliveira <dimitrimatheusdeoliveira@gmail.com>
2023-05-23 19:02:04 -0400

    Update structured

--- minimaltube/main.py
+++ minimaltube/main.py
@@ -15,22 +15,22 @@ class MinimalTube(ctk.CTk):
         self.resizable(width=False, height=False)
 
         # property the theme
-        ctk.set_default_color_theme('themes/cappucino.json') # themes: "standard", "grayline", "mint", "reddish"
+        ctk.set_default_color_theme('minimaltube/themes/standard.json') # themes: "standard", "grayline", "mint", "reddish"
         ctk.set_appearance_mode('system')
 
         # load the images
-        self.arrow = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/push.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/push.png'), size=(45, 45))
-        self.github = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/github.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/github.png'), size=(32, 32))
-        self.instagram = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/instagram.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/instagram.png'), size=(32, 32))
-        self.folder = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/folder.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/folder.png'), size=(32, 32))
+        self.arrow = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/push.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/push.png'), size=(45, 45))
+        self.github = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/github.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/github.png'), size=(32, 32))
+        self.instagram = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/instagram.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/instagram.png'), size=(32, 32))
+        self.folder = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/folder.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/folder.png'), size=(32, 32))
 
-        self.find = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/search.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/search.png'), size=(32, 32))
-        self.resolution = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/hd.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/hd.png'), size=(32, 32))
-        self.download = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/download.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/download.png'), size=(32, 32))
+        self.find = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/search.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/search.png'), size=(32, 32))
+        self.resolution = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/hd.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/hd.png'), size=(32, 32))
+        self.download = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/download.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/download.png'), size=(32, 32))
 
-        self.refresh = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/refresh.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/refresh.png'), size=(32, 32))
-        self.dark = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/moon.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/moon.png'), size=(32, 32))
-        self.light = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/dark/sun.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/light/sun.png'), size=(32, 32))
+        self.refresh = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/refresh.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/refresh.png'), size=(32, 32))
+        self.dark = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/moon.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/moon.png'), size=(32, 32))
+        self.light = ctk.CTkImage(dark_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/dark/sun.png'), light_image=PIL.Image.open('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/light/sun.png'), size=(32, 32))
 
         # next page button
         self.button_push = ctk.CTkButton(self, text=None, image=self.arrow, fg_color='transparent', hover=False, command=lambda: change_buttons('video_download'))
@@ -108,7 +108,7 @@ class MinimalTube(ctk.CTk):
     # add icon to window
     def iconbitmap(self, bitmap):
         self._iconbitmap_method_called = False
-        super().wm_iconbitmap('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/img/icon/menu.ico')
+        super().wm_iconbitmap('C:/Users/dimit/OneDrive/Documentos/Project/MinimalTube/minimaltube/images/icon/menu.ico')
 
 
 if __name__ == "__main__":
